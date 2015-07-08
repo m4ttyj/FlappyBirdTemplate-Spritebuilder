7a9185970db4cf6d4b929280715eae84a3272536
Matt Jenkinson <mjenkinson05@gmail.com>
2015-07-08 19:52:08 +0100

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,17 +10,34 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
+}
+
+// put new methods here
+-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    //This will be called everytime the user touches the screen
+    [character flap];
+    
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
-}
-
-// put new methods here
--(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
-    //This will be called everytime the user touches the screen
-    [character flap];
+    //delta is the time elapsed since the last time it was run.
+    //This is usually 1/60, but can be bigger if the game slows down.
+    
+    //increment the time since the last obstacle was created
+    timeSinceObstacle += delta;
+    
+    //Check to see if 2 seconds have passed
+    if (timeSinceObstacle > 2.0f)
+    {
+        //create another obstacle
+        [self addObstacle];
+        
+        //then reset timer
+        timeSinceObstacle = 0.0f;
+    }
 }
 
 @end
